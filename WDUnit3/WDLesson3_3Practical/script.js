c2b2c61ef164a60d17eb99aa00ce48e22042bd63
cadenw4 <cadenw4@nycstudents.net>
2025-12-22 14:21:33 +0000

    ll

--- WDUnit3/WDLesson3_3Practical/script.js
+++ WDUnit3/WDLesson3_3Practical/script.js
@@ -1,1 +1,21 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+
+function orange(){
+    document.getElementById("highlight").innerHTML = "Single Orange";
+}
+
+function apple(){
+    document.getElementById("highlight").innerHTML="Single Apple";
+}
+
+function kiwi(){
+    document.getElementById("highlight").innerHTML="Single Kiwi"
+}
+
+function strawberry(){
+    document.getElementById("highlight").innerHTML="Single Strawberry"
+}
+
+function banana(){
+    document.getElementById("highlight").innerHTML="Group of Bananas"
+}
