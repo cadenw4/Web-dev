4567906e95b1e25cb84405be35f07f06a5fd5e5e
cadenw4 <cadenw4@nycstudents.net>
2026-04-27 16:26:46 +0000

    update 6.7

--- WDUnit6/WDLesson6_1Practical/WDLesson6_7Practical/script.js
+++ WDUnit6/WDLesson6_1Practical/WDLesson6_7Practical/script.js
@@ -18,6 +18,15 @@ for(let i=0; i < data.length; i+= 1){
   <br>
    <h4>${collisions.crash_date}</h4>
   <h4>${collisions.crash_time}</h4>
+  <br>
+  <p>People Injured: ${collisions.number_of_persons_injured}</p>
+  <p>People Killed: ${collisions.number_of_persons_killed}</p>
+  <p>Pedestrians Injured: ${collisions.number_of_pedestrians_injured}</p>
+  <p>Pedestrians Killed: ${collisions.number_of_pedestrians_killed}</p>
+  <p>Cyclists Injured: ${collisions.number_of_cyclist_injured}</p>
+  <p>Cyclists Killed: ${collisions.number_of_cyclist_killed}</p>
+  <p>Motorists Injured: ${collisions.number_of_motorist_injured}</p>
+  <p>Motorists Killed: ${collisions.number_of_motorist_killed}</p>
   </div>`
 }
 
