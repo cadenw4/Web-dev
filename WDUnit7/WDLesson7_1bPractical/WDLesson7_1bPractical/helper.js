d02c54bacf3368f31f88b85abe83dcde28643312
cadenw4 <cadenw4@nycstudents.net>
2026-05-28 12:56:20 +0000

    aaa

---
+++ WDUnit7/WDLesson7_1bPractical/WDLesson7_1bPractical/helper.js
@@ -0,0 +1,19 @@
+//get( ) accepts an id of an element and returns the actual element with that id. This functions serves to shorten the code required to get an element
+function get(id){
+  return document.getElementById(id);
+}
+
+//Challeng 1: Create a function card that accepts a JSON of a 311 complaint, generates and returns an appropriate card for the complaint
+function card( info ){
+  let b = "";
+  build = `<div class="fitted card">
+          <h1>${info.plate}</h1>
+          </div>
+          `
+  return build;
+}
+
+
+//Challenge 2: Create a function cards that accepts an array of JSON of Open Parking and Camera Violations, generates and return cards for each violation
+
+
